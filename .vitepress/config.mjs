d586a85db1cfa9b92378179c579084daf06e1add
apple <apple@192.168.1.4>
2025-01-10 20:37:18 +0800

    更新内容

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -47,7 +47,7 @@ export default defineConfig({
         text: '产研前端',
         items: [
           { text: ' HTML与CSS', link: '/markdown-examples' },
-          { text: 'JavaScript', link: '/api-examples' }
+          { text: 'JavaScript', link: '/JavaScript' }
         ]
       }
     ],
